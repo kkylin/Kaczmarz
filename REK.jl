ff5c289e32d8b51324a7793c41279c6a040b0895
K K Lin <klin@math.arizona.edu>
2021-01-09 16:40:34 -0700

    REK.jl   : added convergence criterion, but not converging

--- REK.jl
+++ REK.jl
@@ -4,13 +4,15 @@
 ## NM Freris, SIAM J Matrix Anal Appl 34 (2013),
 ## doi:10.1137/120889897
 
+## Still to add: convergence criterion
+
 module REK
 
 using LinearAlgebra
 
 export reksolve, rpick
 
-function reksolve(A,b;maxcount=100)
+function reksolve(A,b;eps = 1e-6, maxcount=100)
     m,n = size(A)
     @assert m >= n
 
@@ -18,19 +20,31 @@ function reksolve(A,b;maxcount=100)
     x = zeros(n)
 
     rowsums  = sum(abs2,A,dims=2)
-    Fnorm    = sum(rowsums)
+    Fnorm2   = sum(rowsums)
+    Fnorm    = sqrt(Fnorm2)
     rowprobs = rowsums / Fnorm
 
     colsums  = sum(abs2,A,dims=1)
     colprobs = colsums / Fnorm
 
+    subcount = 8*min(m,n)
+
     for k = 1:maxcount
-        i = rpick(rowprobs)
-        j = rpick(colprobs)
-        z = z - dot(A[:,j],z)/colsums[j]*A[:,j]
-        x = x + (b[i] - z[i]  - dot(x,A[i,:]))/rowsums[i]*A[i,:]
+        for kk = 1:subcount
+            i = rpick(rowprobs)
+            j = rpick(colprobs)
+            z .= z .- (dot(A[:,j],z)/colsums[j]) .* A[:,j]
+            x .= x .+ ((b[i] - z[i]  - dot(x,A[i,:]))/rowsums[i]) .* A[i,:]
+        end
+        
+        normx = norm(x)
+
+        if ( norm(A*x - b - z) <= eps * Fnorm * normx &&
+             norm(A'*z) <= eps * Fnorm2 * normx )
+            return x,k
+        end
     end
-    return x
+    return x,maxcount
 end
 
 function rpick(probs)
