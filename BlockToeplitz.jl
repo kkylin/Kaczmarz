093009526a36d5e07ecebadcfcb6fc76c2bb4bd5
K K Lin <klin@math.arizona.edu>
2021-01-10 13:45:24 -0700

    revise comments

--- BlockToeplitz.jl
+++ BlockToeplitz.jl
@@ -1,8 +1,11 @@
 module BlockToeplitz
 
-# import Base:getindex,setindex!,view
-import Base:view
+import Base:getindex,setindex!,view
 
-## only need to extend view() for REK.jl to work
+## If we don't mind allocating lots, e.g., view() returns a
+## new vector or matrix every time, then we only need to
+## extend view() for REK.jl to work.  OTOH if we want to be
+## more efficient, then we'll also need to define getindex()
+## and setindex!() for View objects.
 
 end #module
