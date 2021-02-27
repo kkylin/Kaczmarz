db3348ffccdc16412a06bde09d9150a2eb2b1417
K K Lin <klin@math.arizona.edu>
2021-02-27 12:37:45 -0700

    add Util.jl

---
+++ Util.jl
@@ -0,0 +1,41 @@
+module util
+
+import Base.foreach
+
+function foreach(f::Function, range, tag::AbstractString; delay=1.0)
+    let t0::Float64 = time(),
+        tlp = 0.0,
+        count::Int64 = 0,
+        total::Int64 = length(range)
+
+        let imin = minimum(range),
+            imax = maximum(range)
+
+            for i in range
+                f(i)
+                count = count + 1
+                dt = time() - t0
+                if dt >= tlp + delay || i == imin || i == imax
+                    tlp = dt
+                    println("#", tag, ": ", count, "/",
+                            total,
+                            " steps took ",
+                            nicedate(dt), "; eta ",
+                            nicedate((dt/count)*(total-count)))
+                    flush(stdout)
+                end
+            end
+        end
+        return time()-t0
+    end
+end
+
+function foreach(f::Function, range)
+    t0::Float64 = time()
+    for i in range
+        f(i)
+    end
+    return time()-t0
+end
+
+end#module
