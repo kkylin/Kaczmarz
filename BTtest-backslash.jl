3826443623b056bb47911a9a1701baad557a3ee6
K K Lin <klin@math.arizona.edu>
2021-02-27 12:33:21 -0700

    print mem estimates

--- BTtest-backslash.jl
+++ BTtest-backslash.jl
@@ -7,5 +7,6 @@ include("BTtest-params.jl")
 @time output = BTtest.test(N, n, r; method = :backslash)
 
 println("## method: $(output.method)")
+println("## est min mem: $(N*n*(N-r+1)*16*1e-9) GB")
 println("## $(output.iters) iterations")
 println("## norm = $(norm(output.sol))")
--- BTtest-kaczmarz.jl
+++ BTtest-kaczmarz.jl
@@ -7,5 +7,6 @@ include("BTtest-params.jl")
 @time output = BTtest.test(N, n, r; method = :kaczmarz)
 
 println("## method: $(output.method)")
+println("## est min mem: $(N*n*16*1e-9) GB")
 println("## $(output.iters) iterations")
 println("## norm = $(norm(output.sol))")
