494a37f4aaceb1572f393c4751eae2b4f11a16ad
K K Lin <klin@math.arizona.edu>
2021-02-27 12:04:16 -0700

    fix matrix sizes

--- BTtest.jl
+++ BTtest.jl
@@ -2,20 +2,20 @@ module BTtest
 
 using REK,BlockToeplitz,LinearAlgebra
 
-test(m=3,n=3,r=5; flags...) = test(Complex{Float64},m,n,r; flags...)
+test(m=100,n=3,r=5; flags...) = test(Complex{Float64},m,n,r; flags...)
 
 function test(::Type{Float64},m=3,n=3,r=5; flags...)
     A = randn(m,n)
-    b = randn(m)
+    b = randn(m-r+1)
     A = BTMatrix(A,r)
     @time x0 = A\b
     @time x1,k = solve(A,b; flags...)
     (backslash = x0, kaczmarz = x1, itercount = k) 
 end
 
-function test(::Type{Complex{Float64}},m=3,n=3,r=5; flags...)
+function test(::Type{Complex{Float64}},m=100,n=3,r=5; flags...)
     A = randn(m,n) + im*randn(m,n)
-    b = randn(m) + im*randn(m)
+    b = randn(m-r+1) + im*randn(m-r+1)
     A = BTMatrix(A,r)
     @time backslash = A\b
     @time kaczmarz,count = solve(A,b; flags...)
