6fdf62cc210c365cdbaacf4df44e9937de0af9eb
K K Lin <klin@math.arizona.edu>
2021-02-27 12:43:02 -0700

    fix mem est

--- BTtest-backslash.jl
+++ BTtest-backslash.jl
@@ -7,6 +7,6 @@ include("BTtest-params.jl")
 @time output = BTtest.test(N, n, r; method = :backslash)
 
 println("## method: $(output.method)")
-println("## est min mem: $(N*n*(N-r+1)*16*1e-9) GB")
+println("## est min mem: $((N-r+1)*n*r*16*1e-9) GB")
 println("## $(output.iters) iterations")
 println("## norm = $(norm(output.sol))")
