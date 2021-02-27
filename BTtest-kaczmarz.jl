b4110c66c81fb156791a06c47b6e48c58cba7c7e
K K Lin <klin@math.arizona.edu>
2021-02-27 13:13:08 -0700

    print residuals

--- BTtest-kaczmarz.jl
+++ BTtest-kaczmarz.jl
@@ -4,9 +4,15 @@ BTtest.test()
 
 include("BTtest-params.jl")
 
-@time output = BTtest.test(N, n, r; method = :kaczmarz)
+@time output = BTtest.test(N, n, r;
+                           method = :kaczmarz,
+                           eps=1e-3,
+                           maxcount=10,
+                           )
 
 println("## method: $(output.method)")
 println("## est min mem: $(N*n*16*1e-9) GB")
 println("## $(output.iters) iterations")
-println("## norm = $(norm(output.sol))")
+println("## norm = $(sqrt(output.norm2))")
+println("## row-resid = $(sqrt(output.row_resid2))")
+println("## col-resid = $(sqrt(output.col_resid2))")
