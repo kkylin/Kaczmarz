33204f664fd0009225d89349e164a2c43fa2a988
K K Lin <klin@math.arizona.edu>
2021-03-06 22:08:56 -0700

    replace custom foreach() by TimeReporter()

--- Util.jl
+++ Util.jl
@@ -1,32 +1,29 @@
 module Util
 
-import Base.foreach
+export TimeReporter
 
-function foreach(f::Function, range, tag::AbstractString; delay=1.0)
-    let t0::Float64 = time(),
-        tlp = 0.0,
-        count::Int64 = 0,
-        total::Int64 = length(range)
+function TimeReporter(maxcount;
+                      tag="",
+                      period=60.0, # sec
+                      )
 
-        let imin = minimum(range),
-            imax = maximum(range)
+    let t0    = time(),
+        tlast = 0.0,
+        count = 0,
 
-            for i in range
-                f(i)
-                count = count + 1
-                dt = time() - t0
-                if dt >= tlp + delay || i == imin || i == imax
-                    tlp = dt
-                    println("#", tag, ": ", count, "/",
-                            total,
-                            " steps took ",
-                            nicedate(dt), "; eta ",
-                            nicedate((dt/count)*(total-count)))
-                    flush(stdout)
-                end
+        function()
+            count = count + 1
+            dt = time() - t0
+            if dt >= tlast + delay
+                tlast = dt
+                println("#", tag, ": ", count, "/",
+                        maxcount,
+                        " steps took ",
+                        nicedate(dt), "; eta ",
+                        nicedate((dt/count)*(maxcount-count)))
+                flush(stdout)
             end
         end
-        return time()-t0
     end
 end
 
